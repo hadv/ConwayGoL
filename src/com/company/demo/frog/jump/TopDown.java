0f293366a0c64121c6d43c9b4c76e45cc86c1c4c
hadv <dvietha@gmail.com>
2015-10-31 21:01:04 +0700

    Bài #7: Cóc nhảy (Sử dụng top-down dynamic programming)

--- src/com/company/demo/frog/jump/TopDown.java
+++ src/com/company/demo/frog/jump/TopDown.java
@@ -1,38 +1,39 @@
 package com.company.demo.frog.jump;
 
 /**
- * Using <strong>bottom-up</strong> dynamic programing to calculate
+ * Using <strong>top-down</strong> dynamic programing to calculate
  * the total number of way that a frog jumping up stair with <tt>n</tt> steps.
  * The frog can jump 1 step or 2 step for each time.
  */
-public class FrogJump {
-
+public class TopDown {
     public static void main(String[] args) {
         for (int i = 0; i < 100; i++) {
-            System.out.print(jump(i) + " ");
+            System.out.print(jump(i, new long[i+1]) + " ");
         }
     }
 
     /**
-     * Using <strong>bottom-up</strong> dynamic programing to calculate
+     * Using <strong>top-down</strong> dynamic programing to calculate
      * the total number of way that a frog jumping up stair with <tt>n</tt> steps.
      * The frog can jump 1 step or 2 step for each time.
      *
      * @param n the total steps of stair
+     * @param a the array to store the calculated step, the default value <i>zero</i> is mean still not calculated.
      * @return the total number of way that a frog can jump up to the <tt>n</tt> steps stair.
      * If <tt>n < 0</tt>, then return 0.
      */
-    static long jump(int n) {
+    static long jump(int n, long a[]) {
         if (n < 0) return 0;
 
         if (n == 0 || n == 1) return 1;
 
-        long a[] = new long[n+1];
-        a[0] = 1;
-        a[1] = 1;
-        for (int i = 2; i <= n; i++) {
-            a[i] = a[i-1] + a[i-2];
+        if (a[n] != 0) {
+            return a[n];
+        } else {
+            a[n] = jump(n-1, a) + jump(n-2, a);
+            return a[n];
         }
-        return a[n];
     }
+
+
 }
