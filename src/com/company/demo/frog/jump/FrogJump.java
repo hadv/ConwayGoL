28ffa583a11f1cc65a9ad767d3a954a2b98c2440
hadv <dvietha@gmail.com>
2015-10-31 18:43:38 +0700

    Bài #7: Cóc nhảy (Khử đệ quy)

--- src/com/company/demo/frog/jump/FrogJump.java
+++ src/com/company/demo/frog/jump/FrogJump.java
@@ -4,7 +4,7 @@ package com.company.demo.frog.jump;
 public class FrogJump {
 
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 100; i++) {
             System.out.print(jump(i) + " ");
         }
     }
@@ -15,10 +15,19 @@ public class FrogJump {
      *
      * @param n the total steps of stair
      * @return the total number of way that a frog can jump up to the <tt>n</tt> steps stair.
+     * If <tt>n < 0</tt>, then return 0.
      */
-    static long jump(long n) {
+    static long jump(int n) {
+        if (n < 0) return 0;
+
         if (n == 0 || n == 1) return 1;
 
-        return jump(n - 1) + jump(n - 2);
+        long a[] = new long[n+1];
+        a[0] = 1;
+        a[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            a[i] = a[i-1] + a[i-2];
+        }
+        return a[n];
     }
 }
