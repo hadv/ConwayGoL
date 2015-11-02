4b421bde7bc8f8b3d41a8067f1f845129431e359
hadv <dvietha@gmail.com>
2015-11-02 22:19:08 +0700

    Bài cóc nhảy: sử dụng map để lưu cache cho tính toán kiểu top-down.

--- src/com/company/demo/frog/jump/TopDown.java
+++ src/com/company/demo/frog/jump/TopDown.java
@@ -1,5 +1,8 @@
 package com.company.demo.frog.jump;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Using <strong>top-down</strong> dynamic programing to calculate
  * the total number of way that a frog jumping up stair with <tt>n</tt> steps.
@@ -8,7 +11,7 @@ package com.company.demo.frog.jump;
 public class TopDown {
     public static void main(String[] args) {
         for (int i = 0; i < 100; i++) {
-            System.out.print(jump(i, new long[i+1]) + " ");
+            System.out.print(jump(i, new HashMap<Integer, Long>()) + " ");
         }
     }
 
@@ -18,20 +21,20 @@ public class TopDown {
      * The frog can jump 1 step or 2 step for each time.
      *
      * @param n the total steps of stair
-     * @param a the array to store the calculated step, the default value <i>zero</i> is mean still not calculated.
+     * @param cache the map to store the calculated step.
      * @return the total number of way that a frog can jump up to the <tt>n</tt> steps stair.
      * If <tt>n</tt> is less than 0, then return 0.
      */
-    static long jump(int n, long a[]) {
+    static long jump(int n, Map<Integer, Long> cache) {
         if (n < 0) return 0;
 
         if (n == 0 || n == 1) return 1;
 
-        if (a[n] != 0) {
-            return a[n];
+        if (cache.containsKey(n)) {
+            return cache.get(n);
         } else {
-            a[n] = jump(n-1, a) + jump(n-2, a);
-            return a[n];
+            cache.put(n, jump(n-1, cache) + jump(n-2, cache));
+            return cache.get(n);
         }
     }
 }
