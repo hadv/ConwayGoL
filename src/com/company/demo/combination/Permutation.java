284bfc01ba6cd50aa8509fb2b0e1b9fcbbc3c77b
hadv <dvietha@gmail.com>
2015-11-09 23:26:25 +0700

    ticket #8: Hoàn thành bài tập abc

--- src/com/company/demo/combination/Permutation.java
+++ src/com/company/demo/combination/Permutation.java
@@ -1,81 +1,135 @@
 package com.company.demo.combination;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
+/**
+ * The demo permutation string program. Given an input string
+ * then print all the k-length permutation string f
+ */
 public class Permutation {
-    public static void main(String[] args) {
-        StringPermutation sp = new StringPermutation("12345");
 
-        int i = 1;
-        System.out.println(i + "\t" + sp.toString());
-        while (sp.hashNext()) {
-            System.out.println(++i + "\t" + sp.next());
+    /**
+     * The end-point the demo program.
+     * @param args input parameter of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("----------- f1 ------------");
+        List<String> output = f1("abc");
+        for (String s: output) {
+            System.out.println(s);
         }
+
+        System.out.println("----------- f2 ------------");
+        output = f2("abc");
+        for (String s: output) {
+            System.out.println(s);
+        }
+    }
+
+    /**
+     * Calculate all the k-length permutation of given string, 1 <= k <= n.
+     *
+     * @param s the given input string
+     * @return list of all k-length permutation
+     */
+    public static List<String> f1(String s) {
+        List<String> output = new ArrayList<String>();
+        int len = s.length();
+        for (int i = 1; i <= len; i++) {
+            StringPermutation sp = new StringPermutation(i, s);
+            sp.combination(0);
+            output.addAll(sp.getOutputList());
+        }
+        return output;
+    }
+
+    /**
+     * Calculate the n-length of permutation of given string.
+     *
+     * @param s the given input string
+     * @return list of all n-length permutation
+     */
+    public static List<String> f2(String s) {
+        int len = s.length();
+        StringPermutation sp = new StringPermutation(len, s);
+        sp.combination(0);
+        return sp.getOutputList();
     }
 }
 
 /**
- *
+ * Calculate all the permutation string with k-length from input n-length string, <tt>1 <= k <= n<tt/>.
+ * Then return a list of k-length permutation string.
  */
 class StringPermutation {
-    private String[] permutationArray;
 
-    public StringPermutation(String str) {
+    // Character array to present the input string
+    private char[] input;
+
+    // The boolean array to store the used flag of an item
+    private boolean[] used;
+
+    // The current permutation output from the input string
+    private char[] output;
+
+    // Output k-length
+    private int k;
+
+    // Output result all of the k-length permutation of input string
+    private List<String> outputList;
+
+    /**
+     * Construct to create an instance of {@code StringPermutation} class
+     * to calculate all the k-length permuration of given input string {@code str}
+     *
+     * @param k the input length of the permutation
+     * @param str the given input string
+     */
+    public StringPermutation(int k, String str) {
         if (str == null || str.trim().length() == 0) {
             throw new IllegalArgumentException();
         }
         int len = str.length();
-        permutationArray = new String[len];
+        input = new char[len];
+        used = new boolean[len];
         for (int i = 0; i < len; i++) {
-            permutationArray[i] = str.substring(i, i+1);
+            input[i] = str.charAt(i);
+            used[i] = false;
         }
-        Arrays.sort(permutationArray);
+
+        this.k = k;
+        output = new char[k];
+        outputList = new ArrayList<String>();
     }
 
-    public boolean hashNext() {
-        int len = permutationArray.length;
-
-        for (int i = len - 1; i > 0; i--) {
-            if (permutationArray[i].compareTo(permutationArray[i-1]) > 0) {
-                return true;
+    /**
+     * A back-tracking method to calculate all the k-length permutation of input n-length string.
+     *
+     * @param i the i-th item in the k-length
+     */
+    public void combination(int i) {
+        int len = input.length;
+        for (int j = 0; j < len; j++) {
+            if (!used[j]) {
+                output[i] = input[j];
+                if (i == k - 1) {
+                    outputList.add(new String(output));
+                } else {
+                    used[j] = true;
+                    combination(i + 1);
+                    used[j] = false;
+                }
             }
         }
-        return false;
     }
 
-    public String next() {
-        if (!hashNext()) {
-            return null;
-        }
-
-        int len = permutationArray.length;
-        int idx = -1;
-        for (int i = len - 1; i > 0; i--) {
-            if (permutationArray[i].compareTo(permutationArray[i-1]) > 0) {
-                idx = i;
-                break;
-            }
-        }
-
-        Arrays.sort(permutationArray, idx, len);
-
-        for (int i = idx; i < len; i++) {
-            if (permutationArray[i].compareTo(permutationArray[idx-1]) > 0) {
-                String tmp = permutationArray[i];
-                permutationArray[i] = permutationArray[idx - 1];
-                permutationArray[idx - 1] = tmp;
-                break;
-            }
-        }
-        return toString();
-    }
-
-    public String toString() {
-        StringBuffer sb = new StringBuffer();
-
-        for (int i = 0; i < permutationArray.length; i++) {
-            sb.append(permutationArray[i]);
-        }
-        return sb.toString();
+    /**
+     * Return the list of k-length permutation of input string.
+     *
+     * @return the output result of k-length permutation string.
+     */
+    public List<String> getOutputList() {
+        return outputList;
     }
 }
